5c693c869933f2df0c721c551aaddd51aae8f33c
kabita kumari <74282784+manvimahi20@users.noreply.github.com>
2021-09-02 14:49:13 +0530

    Add files via upload

---
+++ multi_callback.js
@@ -0,0 +1,18 @@
+function add(a){
+    console.log("kavita")
+    a(again)
+}
+function callme(b){
+    console.log("manvi")
+    b(fun2)
+}
+function again(c){
+    console.log("banglor")
+    c()
+    
+}
+function fun2(){
+    console.log("i am in navgurukul")
+}
+
+add(callme)
