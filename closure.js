d248bed8e73a85b57fd77d6d015016a9c605deb6
kabita kumari <74282784+manvimahi20@users.noreply.github.com>
2021-09-22 12:14:41 +0530

    Add files via upload

--- closure.js
+++ closure.js
@@ -10,19 +10,19 @@
 
 
 
-// const outerfun=(a)=>{
-//     var b=10;
-//     const innerfun=(c)=>{
-//         let sum=a+b+c;
-//         return (sum)
-//     }
-//     // innerfun(4);
-//     return innerfun(4)
-// } 
-// const num=outerfun(5)
-// console.log(num)
-// // num()
-// // console.log(b)
+const outerfun=(a)=>{
+    var b=10;
+    const innerfun=(c)=>{
+        let sum=a+b+c;
+        return (sum)
+    }
+    // innerfun(4);
+    return innerfun
+} 
+const num=outerfun(5)
+console.log(num(4))
+// num()
+// console.log(b)
 
 
 
@@ -40,45 +40,45 @@
 
 
 
-// function fristname(){
-//     let name="manvi";
-//     function lastname(){
-//         let name1="mahi"
-//         return name+name1
-//     }
-//     return lastname()
-// }
-// const main=fristname();
-// console.log(main)
+function fristname(){
+    let name="manvi";
+    function lastname(){
+        let name1="mahi"
+        return name+name1
+    }
+    return lastname
+}
+const main=fristname();
+console.log(main())
 
 
 
-// function myFunction() {
-//     let a = 4;
-//     function secFun(){
-//         return a * a;
-//     }
-//     return secFun
+function myFunction() {
+    let a = 4;
+    function secFun(){
+        return a * a;
+    }
+    return secFun
     
-// }
-// // console.log(myFunction())
-// const main=myFunction()
-// console.log(main)
+}
+// console.log(myFunction())
+const main=myFunction()
+console.log(main())
 // main()
 
 
-// function name(){
-//     var name1="manvi"
-//     function add(){
-//         console.log(name1)
-//     }
-//     return add()
-// }
-// const main=name()
-// console.log(main)
+function name(){
+    var name1="manvi"
+    function add(){
+        console.log(name1)
+    }
+    return add
+}
+const main=name()
+console.log(main())
 
 
-var new_array = arr.filter(function callback(element, index, array) {
-    // Return true or false
-}[ thisArg])
-console.log(new_array)
+// var new_array = arr.filter(function callback(element, index, array) {
+//     // Return true or false
+// }[ thisArg])
+// console.log(new_array)
